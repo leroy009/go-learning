91c4b2d9d0dcd38ee22dc6a705943a84c447ea97
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-12 17:02:03 +0200

    Calculator using functions

--- 002-simple-profic-calculator/main.go
+++ 002-simple-profic-calculator/main.go
@@ -1,28 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var revenue, expenses, taxRate float64
+	// var revenue, expenses, taxRate float64
+	var revenue, expenses float64
 
-	fmt.Print("Input your Revenue: ")
-	fmt.Scan(&revenue)
+	// fmt.Print("Input your Revenue: ")
+	// fmt.Scan(&revenue)
 
-	fmt.Print("Input your Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Input your Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expenses
-	profit := ebt * (1- (taxRate/100))
-	ratio := ebt/profit
+	getUserInput("Input your Revenue", &revenue)
 	
+	// fmt.Print("Input your Expenses: ")
+	// fmt.Scan(&expenses)
+	getUserInput("Input your Expenses", &expenses)
+	
+	// fmt.Print("Input your Tax Rate: ")
+	// fmt.Scan(&taxRate)
+	// getUserInput("Input your Tax Rate", &taxRate)
+	taxRate:= getuserInputProper("Input your Tax Rate :")
+
+	// ebt := revenue - expenses
+	// profit := ebt * (1- (taxRate/100))
+	// ratio := ebt/profit	
+	ebt, profit, ratio := calcualteProfits(revenue, expenses, taxRate)
 
 	fmt.Println("Earniong before Tax: ", ebt)
 	fmt.Println("Earnings after Tax: ", profit)
 	fmt.Println("Ratio: ", ratio)
 
-	fmt.Printf("Earnings before Tax: %v \nEarnings after Tax: %v \nRatio: %v", ebt, profit, ratio)
+	fmt.Printf("Earnings before Tax: %.2f \nEarnings after Tax: %.2f \nRatio: %.3f", ebt, profit, ratio)
+}
 
+func getUserInputErrorProne(message string, value *float64) {
+	fmt.Print(message + " :")
+	fmt.Scan(value)
+}
+
+func getuserInputProper(message string) float64 {
+	var userInput float64
+	fmt.Print(message)
+	fmt.Scan(&userInput)
+
+	return userInput
+}
+
+func getUserInput(message string, value *float64) {
+  fmt.Print(message + " :")
+  _, err := fmt.Scan(value)
+  if err != nil {
+    fmt.Println("Error: Invalid input. Please enter a number.")
+    getUserInput(message, value) // Recursively call the function to get valid input
+  }
+}
+
+func calcualteProfits(revenue, expense, taxRate float64) (ebt, profit, ratio float64 ) {
+	ebt = revenue - expense
+	profit = ebt * (1- (taxRate/100))
+	ratio = ebt/profit	
+
+	return ebt, profit, ratio
 }
