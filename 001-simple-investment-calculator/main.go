28d53ace5e7de75056a2198d00fa72f088f1564f
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-12 14:28:47 +0200

    Getting user input.

--- 001-simple-investment-calculator/main.go
+++ 001-simple-investment-calculator/main.go
@@ -10,6 +10,8 @@ func main() {
 	var investmentAmount, years float64 = 1200, 5
 	expectedReturnRate := 12.36
 
+	fmt.Scan(&investmentAmount)
+
 	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/ 100), years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 
