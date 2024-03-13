e46ab4e442faed65c5221c9cb1c4836f2ef47eaf
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-13 06:10:13 +0200

    go loops

--- 003-basic-bank/main.go
+++ 003-basic-bank/main.go
@@ -4,51 +4,53 @@ import "fmt"
 
 func main () {
 	var accountBlace float64 = 1500
-	
 	fmt.Println("Welcome to Leroy Bank")
-	fmt.Println("What would you like to do today?")
-	fmt.Println("1. Check your balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
 
-	option := getUserInput("Enter your option: ")
+	for {
+		fmt.Println("What would you like to do today?")
+		fmt.Println("1. Check your balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+
+		option := getUserInput("Enter your option: ")
 
 
-	if option == 1 {
-		fmt.Printf("Your account balance is %.2f \n", accountBlace)
-	} else if option == 2 {
-		amountToDeposit := getUserInput("Enter the amount to deposit: ")
+		if option == 1 {
+			fmt.Printf("Your account balance is %.2f \n", accountBlace)
+		} else if option == 2 {
+			amountToDeposit := getUserInput("Enter the amount to deposit: ")
 
-		if amountToDeposit <= 0 {
-			fmt.Println("Invalid amount")
-			return
+			if amountToDeposit <= 0 {
+				fmt.Println("Invalid amount")
+				continue
+			}
+
+			accountBlace = accountBlace + amountToDeposit
+			fmt.Printf("Your new account balance is %.2f \n", accountBlace)
+		} else if option == 3 {
+			amountToWithdraw := getUserInput("Enter the amount to withdraw: ")
+
+			if amountToWithdraw <= 0 {
+				fmt.Println("Invalid amount")
+				continue
+			}
+
+			if amountToWithdraw > accountBlace {
+				fmt.Printf("Invalid amount. You can't withdraw more your account balance: %.2f\n", accountBlace)
+				continue
+			}
+
+			accountBlace = accountBlace - amountToWithdraw
+			fmt.Printf("Your new account balance is %.2f \n", accountBlace)
+		} else if option == 4 {
+			fmt.Println("Goodbye!")	
+			break
 		}
+		fmt.Printf("Your option: %v", option)
+	} 
 
-		accountBlace = accountBlace + amountToDeposit
-		fmt.Printf("Your new account balance is %.2f \n", accountBlace)
-	} else if option == 3 {
-		amountToWithdraw := getUserInput("Enter the amount to withdraw: ")
-
-		if amountToWithdraw <= 0 {
-			fmt.Println("Invalid amount")
-			return
-		}
-		
-		if amountToWithdraw > accountBlace {
-			fmt.Printf("Invalid amount. You can't withdraw more your account balance: %.2f\n", accountBlace)
-			return
-		}
-
-		accountBlace = accountBlace - amountToWithdraw
-		fmt.Printf("Your new account balance is %.2f \n", accountBlace)
-	} else if option == 4 {
-		fmt.Println("Goodbye!")	
-	}
-
-
-
-	fmt.Printf("Your option: %v", option)
+	fmt.Println("Thank you for using this good bank")
 }
 
 func getUserInput(message string) float64 {
