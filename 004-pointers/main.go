d3db5cd0a1fbe98f31b35ce1b97bf2d3b1169d3a
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-15 06:05:41 +0200

    Using pointers for data mutation

--- 004-pointers/main.go
+++ 004-pointers/main.go
@@ -5,20 +5,20 @@ import "fmt"
 func main () {
 	age := 70
 
-	var agePointer *int
 	// agePointer := &age
+	var agePointer *int
 	agePointer = &age
 
 	fmt.Println("Age :", age)
 	fmt.Println("Age Pointer Address Value :", agePointer)
 	fmt.Println("Age Pointer Value :", *agePointer)
 	
-	adultYears := getAdultYears(agePointer)
-	fmt.Println("Adult Age :", adultYears)
+	editAgeToAdultYears(agePointer)
+	// fmt.Println("Adult Age :", adultYears)
 	fmt.Println("Age :", age)
 	fmt.Println("Age Pointer Value :", *agePointer)
 }
 
-func getAdultYears(age *int) int {
-	return *age -18
+func editAgeToAdultYears(age *int) {
+	*age = *age -18
 }
