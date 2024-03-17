46346b7fdcf6a71f10f4fe3a30e8b6ef090f26e0
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-17 17:35:46 +0200

    Exposing structs as package and its methods and contrsuctors

--- 005-structs-custom-types/main.go
+++ 005-structs-custom-types/main.go
@@ -1,59 +1,33 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"leroy.africa/leroy/structs-custom-types/user"
 )
 
-type User struct {
-	firstName string
-	lastName string
-	birthDate string
-	createdAt time.Time
-}
 
-func (u User) outputUserData() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-func (u *User) clearUsername() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthDate string) (*User, error) {
-	if firstName == "" || lastName == "" || birthDate == "" {
-		return nil, errors.New("First name, last name, and birthdate are required")
-	}
-	return &User {
-		firstName : firstName,
-		lastName : lastName,
-		birthDate : birthDate,
-		createdAt : time.Now(),
-	}, nil
-} 
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var theUser *User	
-	theUser, err := newUser("Leroy", "Royal", "01/01/2002")
-	anotherUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	var theUser *user.User	
+	theUser, err := user.NewUser("Leroy", "Royal", "01/01/2002")
+	anotherUser, err := user.NewUser(userFirstName, userLastName, userBirthdate)
 
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 
-	theUser.outputUserData()
-	anotherUser.outputUserData()
-	theUser.clearUsername()
-	anotherUser.clearUsername()
-	anotherUser.outputUserData()
-	theUser.outputUserData()
+	theUser.OutputUserData()
+	anotherUser.OutputUserData()
+	theUser.ClearUsername()
+	anotherUser.ClearUsername()
+	anotherUser.OutputUserData()
+	theUser.OutputUserData()
 
 }
 
