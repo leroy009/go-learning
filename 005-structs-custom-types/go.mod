f0e0f64c28dd7b3de2c703c19d81507b7ca2f6b9
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-17 13:01:45 +0200

    Defining struct type

---
+++ 005-structs-custom-types/go.mod
@@ -0,0 +1,3 @@
+module leroy.africa/leroy/structs-custom-types
+
+go 1.22.1
---
+++ 005-structs-custom-types/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName string
+	birthDate string
+	createdAt time.Time
+}
+
+func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	
+
+	fmt.Println(firstName, lastName, birthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
