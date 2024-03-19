ad4f0dc9312baedbb2c2aedfbf8fb90a8c68651a
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-19 18:52:32 +0200

    Generics

--- 006-notes/main.go
+++ 006-notes/main.go
@@ -40,6 +40,9 @@ func main() {
 	printAnything([]int{1, 2, 3, 4, 5})
 
 
+	restult := add(1, 2)
+	fmt.Println(restult)
+
 	title, content, err := getNoteData()
 	
 	if (err != nil) {
@@ -81,29 +84,8 @@ func main() {
 	}
 }
 
-func add(a, b interface{}) interface{} {
-	aInt, aIsInt := a.(int)
-	bInt, bIsInt := b.(int)
-
-	if aIsInt && bIsInt {
-		return aInt + bInt
-	}
-
-	aFloat, aIsFloat := a.(float64)
-	bFloat, bIsFloat := b.(float64)	
-
-	if aIsFloat && bIsFloat {
-		return aFloat + bFloat
-	}
-
-	aString, aIsString := a.(string)
-	bString, bIsString := b.(string)
-
-	if aIsString && bIsString {
-		return aString + bString
-	}
-
-	return nil
+func add[T int|float64|string](a, b T) T {
+	return a + b
 }
 
 func printAnything(value interface{}) {
