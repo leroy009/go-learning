9ee392a6ad4f93b9410a7cadb6762232b2da94b1
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-19 18:36:02 +0200

    interface, Dynamics Types and limitations.

--- 006-notes/main.go
+++ 006-notes/main.go
@@ -81,6 +81,31 @@ func main() {
 	}
 }
 
+func add(a, b interface{}) interface{} {
+	aInt, aIsInt := a.(int)
+	bInt, bIsInt := b.(int)
+
+	if aIsInt && bIsInt {
+		return aInt + bInt
+	}
+
+	aFloat, aIsFloat := a.(float64)
+	bFloat, bIsFloat := b.(float64)	
+
+	if aIsFloat && bIsFloat {
+		return aFloat + bFloat
+	}
+
+	aString, aIsString := a.(string)
+	bString, bIsString := b.(string)
+
+	if aIsString && bIsString {
+		return aString + bString
+	}
+
+	return nil
+}
+
 func printAnything(value interface{}) {
 	switch value.(type) {
 	case int:
