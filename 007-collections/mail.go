7c223b59ecfa98491938e0ad8af9e75c65002872
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-20 04:45:32 +0200

    working with arrays

--- 007-collections/mail.go
+++ 007-collections/mail.go
@@ -9,6 +9,15 @@ type Prodcut struct {
 }
 
 func main() {
+	var prodNames [4]string = [4]string{"Laptop", "Mouse", "Keyboard"}
 	prices := [5]float64{15.59, 12.50, 9.59, 5.99}
 	fmt.Println(prices)
+	fmt.Println(prodNames)
+
+	fmt.Println(prices[2])
+
+	prodNames[3] = "Monitor"
+	fmt.Println(prodNames[3])
+
+
 }
