25f48a47f6a2784be4bf51f840de95f18df92e6b
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-20 18:55:58 +0200

    proj: loading data from a file

--- 009-price-calculator/main.go
+++ 009-price-calculator/main.go
@@ -1,21 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"leroy.africa/leroy/price-calulator/prices"
+)
 
 func main() {
-	var prices []float64 = []float64{10,20,30,33.5}
 	taxRates := []float64{0.10, 0.15, 0.20, 0.25}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxInlcudedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxInlcudedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxInlcudedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
 
 }
--- 009-price-calculator/prices/prices.go
+++ 009-price-calculator/prices/prices.go
@@ -1,9 +1,52 @@
 package prices
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 type TaxIncludedPriceJob struct {
 	TaxRate float64
 	InputPrices []float64
 	TaxIncludedPrices map[string]float64 
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err :=os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+
+	var lines []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Reading the file contents failed!")
+		fmt.Printf("Error: %s\n", err)
+		file.Close()
+		return
+	}
+
+
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+	
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("price %.2f", price)] = price * (1 + job.TaxRate)
+	}	
+
+	fmt.Println(result)
 }
 
 func NewTaxIncludedPriceJob (taxRate float64) *TaxIncludedPriceJob {
