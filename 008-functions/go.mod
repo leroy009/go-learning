dae4890869f61e0f459e86450215b212ad22bc8a
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-20 13:57:50 +0200

    Functions as Values & Fucntions Types

---
+++ 008-functions/go.mod
@@ -0,0 +1,3 @@
+module leroy.africa/leroy/functions
+
+go 1.22.1
---
+++ 008-functions/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	numbers := []int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
+	doubledNumbers := doubleNumber(&numbers)
+	fmt.Println(doubledNumbers)
+
+	tripledNumbers := transformNumbers(&numbers, triple)
+	fmt.Println(tripledNumbers)
+
+	// numbers := []int{1, 2, 3, 4, 5}
+	// doubledNumbers := transformNumbers(numbers, double)
+	// fmt.Println(doubledNumbers)
+}
+
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
+	transformedNumbers := []int{}
+
+	for _, number := range *numbers {
+		transformedNumbers = append(transformedNumbers, transform(number))
+	}
+	return transformedNumbers
+}
+
+func doubleNumber(numbers *[]int) []int {
+	doubledNumbers := []int{}
+	for _, number := range *numbers {
+		doubledNumbers = append(doubledNumbers, double(number))
+	}
+
+	return doubledNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
