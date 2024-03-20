04cb790dcf8f6bf9f42c5d4eb21991f9a67b7b62
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-20 05:16:36 +0200

    exercise. using slices, arrays, and dynamic arrays

--- 007-collections/mail.go
+++ 007-collections/mail.go
@@ -9,6 +9,49 @@ type Prodcut struct {
 }
 
 func main() {
+	// 1) Create a new array (!) that contains three hobbies you have
+	hobboes := []string{"Swimming", "Hunting", "Reading"}
+	// 		Output (print) that array in the command line.
+	fmt.Println(hobboes)
+
+	// 2) Also output more data about that array:
+	//		- The first element (standalone)
+	fmt.Println(hobboes[0])
+	//		- The second and third element combined as a new list
+	secondAndThird := hobboes[1:3]
+	fmt.Println(secondAndThird)
+	// 3) Create a slice based on the first element that contains
+	//		the first and second elements.
+	firstTwo := hobboes[:2]
+	fmt.Println(firstTwo)
+	//		Create that slice in two different ways (i.e. create two slices in the end)
+	firstTwoAgain := hobboes[0:2]
+	fmt.Println(firstTwoAgain)
+	// 4) Re-slice the slice from (3) and change it to contain the second
+	//		and last element of the original array.
+	secondAndLast := hobboes[1:3]
+	fmt.Println(secondAndLast)
+	// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
+	goals := []string{"Learn Go", "Build projects"}
+	fmt.Println(goals)
+	// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
+	goals[1] = "Learn more Go"
+	goals = append(goals, "Get a job")
+	// 7) Bonus: Create a "Product" struct with title, id, price and create a
+	//		dynamic list of products (at least 2 products).
+	products := []Prodcut{
+		{id: "p1", name: "Product 1", price: 15.59},
+		{id: "p2", name: "Product 2", price: 12.50},
+	}
+	fmt.Println(products)
+	
+	//		Then add a th ird product to the existing list of products.
+	newProduct := Prodcut{id: "p3", name: "Product 3", price: 9.59}
+	products = append(products, newProduct)
+	fmt.Println(products)
+}
+
+func mainDynamicSlices() {
 	prices := []float64{15.59, 12.50, 9.59, 5.99}
 	fmt.Println(prices[0])
 	fmt.Println(prices[1:2])
