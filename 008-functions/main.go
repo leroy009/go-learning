d2ab4099f136cf3fecba0cecb86a0162b5d8727c
leroy <16562492+leroy009@users.noreply.github.com>
2024-03-20 18:22:35 +0200

    splittinh arrays and slices into parameter values

--- 008-functions/main.go
+++ 008-functions/main.go
@@ -4,12 +4,15 @@ import "fmt"
 
 func main() {
 
-	// numbers := []int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 	sum := sumup(1, 2, 3, 4, 5, -6)
 	fmt.Println(sum)
+	
+	anotherSum := sumup(1, numbers...)
+	fmt.Println(anotherSum)
 }
 
-func sumup(numbers ...int) int {
+func sumup(startingValue int, numbers ...int) int {
 
 	sum := 0
 	for _, number := range numbers {
